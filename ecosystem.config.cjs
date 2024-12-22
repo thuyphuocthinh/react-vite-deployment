dd391aa93ea013a747ba4b7826056c283ffd94d5
ThuyPhuocThinh <thuyphuocthinhtpt1@gmail.com>
2024-12-22 10:48:59 +0700

    added pm2

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "DuAnReactVite",
+      cwd: "E:/Career/Software_Engineer/Web/Deployment/project/react-vite", // Path to your project
+      script: "npm",
+      args: "run preview",
+    },
+  ],
+};
